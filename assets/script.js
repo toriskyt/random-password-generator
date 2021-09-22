e41613b21f084c49b1a2b1ba9c22da20fabf506e
Tracy Torisky <mommers1@verizon.net>
2021-09-22 15:58:44 -0400

    work on random pulls

--- assets/script.js
+++ assets/script.js
@@ -10,11 +10,14 @@ function writePassword() {
   
 }
 
+// begin function
 function generatePassword() {
   var options = askUser();
   console.log(options)
-  return "a"
+  return "Your password is:  "
 }
+
+// identify variable values
 var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ];
 var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
@@ -22,6 +25,7 @@ var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
 var numeralsArr = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialSymbolsArr = ["!", "@", "#", "$", "%", "&", "*"];
 
+// ask user for preferences for password
 function askUser() {
   var lowerCase = confirm("Would you like a lowercase letter?");
   var upperCase = confirm("Would you like an uppercase letter?");
@@ -29,6 +33,9 @@ function askUser() {
   var specialSymbols = confirm("Would you like a special character?");
   var passLength = parseInt(prompt("How long would you like the password to be?"));
   console.group(lowerCase, upperCase, numerals, specialSymbols, passLength);
+
+  // password needs to be greater than "8" and less than "128"
+  // if a number less than "8" or greater than "128" is chosen, the alert will ask for a valid length
   if(passLength < 8 || passLength >= 128) {
     alert("Please select a valid length.");
     return askUser(); 
@@ -39,18 +46,6 @@ function askUser() {
   console.log(optionsObj)
     return optionsObj
 }
-
-
-
-
-// var newPassword = "Your new password is  .";
-    // if(newPassword) [
-        // newPassword = lowerCase + upperCase + numerals + specialSymbols[Math.floor(Math.random() * 10)]
-
-    // ]
-
-// alert: ("The password is:");
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
